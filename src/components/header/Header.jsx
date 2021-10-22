6f4c0c96c2e819a9881bd6eb7c82958899cfd9a5
Rafael <rafaelribeirodasilva7@gmail.com>
2021-10-22 17:54:04 -0300

    novos arquivos

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+function Header(){
+    return(
+        <div className="header">
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut blanditiis illo possimus accusamus commodi tenetur debitis facilis. Ipsum, aspernatur. In magnam minus nemo. Repellat incidunt, possimus quidem voluptas consectetur commodi!</p>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/components/menu/Menu.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './Menu.css';
+
+function Menu(){
+    return(
+        <>
+            <ul>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+            </ul>
+        </>
+    )
+}
+
+export default Menu;
